83ea2944dcf365fb6a978449e2df369c5183a047
sarrow104 <sarrow104@gmail.com>
2016-02-02 14:48:16 +0800

    cmake updated for more exampels

--- src/tools/logsys/logdef.cpp
+++ src/tools/logsys/logdef.cpp
@@ -2,11 +2,11 @@
 ///   (Home at https://github.com/NingLeixueR/middleware/)
 
 namespace middleware {
-	namespace tools{
+  namespace tools{
 
-		int g_log_socked_io_id;
+    int g_log_socked_io_id;
 
-	} //namespace tools
+  } //namespace tools
 }//namespace middleware 
 
 /* vim: set expandtab ts=2 sw=2 sts=2 tw=100: */
