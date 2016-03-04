dc424949435fc4ec72823b6b3446217dd8e9fca7
sarrow104 <sarrow104@gmail.com>
2016-03-04 21:16:37 +0800

    fuzzy charset problem on example/example_segmentation_pack.cpp

--- example/example_segmentation_pack.cpp
+++ example/example_segmentation_pack.cpp
@@ -26,7 +26,7 @@ int main()
   abc la;
   
 
-  /** 閺佺増宓侀弽鐓庣础 = | len(sizeof(uint16_t)) | data(len) |  */
+  /** 数据格式 = | len(sizeof(uint16_t)) | data(len) |  */
   char buff[1024000];
   char bufftemp[16];
   
